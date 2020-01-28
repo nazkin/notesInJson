815185de161fe8cce050f0efc171566f573771e0
nazkin <nazar.kinash93@gmail.com>
2020-01-28 00:44:31 -0500

    only need to fix one more route for json file to work correctly

--- db/data.js
+++ db/data.js
@@ -5,13 +5,7 @@
 ];
 module.exports = data;
 
-// fs.readFile('./customer.json', 'utf8', (err, jsonString) => {
-//     if (err) {
-//         console.log("File read failed:", err)
-//         return
-//     }
-//     console.log('File data:', jsonString) 
-// });
+
 // parse json
 // var jsonObj = JSON.parse(jsonData);
 // console.log(jsonObj);
@@ -20,9 +14,3 @@ module.exports = data;
 // var jsonContent = JSON.stringify(jsonObj);
 // console.log(jsonContent);
 
-// fs.writeFile("data.json", jsonContent, 'utf8', function (err) {
-//     if (err) {
-//         console.log("An error occured while writing JSON Object to File.");
-//         return console.log(err);
-//     }
-// });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,38 @@
 let notesData = require("../db/data");
+const fs = require('fs');
 module.exports = function(app) {
 
     app.get('/api/notes', (req,res)=>{
-        res.json(notesData);
+        fs.readFile('./db/data.json', 'utf8', (err, jsonNotes) => {
+             if (err) {
+                 console.log("File read failed:", err)
+                 return
+             }
+             res.json(JSON.parse(jsonNotes));
+        });
+        // res.json(notesData);
     }); 
     app.post('/api/add', (req,res)=>{
+        let jsonArr = [];
         notesData.push(req.body);
-        res.json(true);
+        jsonArr = [...notesData];
+        fs.writeFile("./db/data.json", JSON.stringify(jsonArr), 'utf8', function (err) {
+             if (err) {
+                    console.log("An error occured while writing JSON Object to File.");
+                    return console.log(err);
+                }
+            res.json(true);
+        });
+        
     });
-    app.delete('/api/delete', (req,res)=>{
-        res.send("Note deleted homie");
+    app.delete('/api/delete/:id', (req,res)=>{
+        for(let i = 0; i < notesData.length; i++){
+            if(notesData[i].title == req.params.id){
+                notesData.splice(i);
+                res.json(true);
+            }
+        }
+
+        
     }); 
 }
